d8ae814e3b90d26e5e2ece40c8f96033b9781cdd
Gzk <40055247+Gzk738@users.noreply.github.com>
2020-07-14 09:40:12 +0800

    编译成功

--- APP/sensor/2_wind/Wind_Hal.c
+++ APP/sensor/2_wind/Wind_Hal.c
@@ -93,12 +93,12 @@ void Init_Timer1(unsigned short delayMS)
     TIM_MasterConfigTypeDef sMasterConfig;
 
     htim1.Instance = TIM1;
-    htim1.Init.Prescaler = 1000;
+    htim1.Init.Prescaler = 7199;
     htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
-    htim1.Init.Period = 20000;
+    htim1.Init.Period = 4199;
     htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
     htim1.Init.RepetitionCounter = 0;
-    htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+    htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
     if (HAL_TIM_Base_Init(&htim1) != HAL_OK)
     {
         _Error_Handler(__FILE__, __LINE__);
