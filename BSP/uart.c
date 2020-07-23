b71b4e5a7f71e0f3a7c63392cc9ac0cc655d6c8c
Gzk <40055247+Gzk738@users.noreply.github.com>
2020-07-23 15:32:28 +0800

    删除USART3_RX

--- BSP/uart.c
+++ BSP/uart.c
@@ -922,7 +922,6 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
         {
             if(huart->Instance == USART3)
             {
-                //USART3_RX();
                 return;
             }
         }
